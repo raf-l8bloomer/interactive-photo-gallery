adfc5ba76753f164ee4f23cc1c5d970251827695
rafael.lumang <lumang.rafael@gmail.com>
2023-11-17 16:30:58 -0800

    added search filter and adjusted minimum sizes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 baguetteBox.run('.gallery', {
     // Custom options
 });
+
+const search = document.querySelector('#search');
+const images = document.querySelectorAll('a.image')
+console.log(images)
+
+search.addEventListener('keyup', (e) => {
+    let currentValue = e.target.value.toLowerCase();
+    console.log('This is the search value:', currentValue);
+
+    images.forEach(image => {
+        console.log(image.dataset.caption)
+        if (image.dataset.caption.toLowerCase().includes(currentValue)) {
+            image.style.display = "flex"
+        } else {
+            image.style.display = "none";
+        }
+    })
+})
